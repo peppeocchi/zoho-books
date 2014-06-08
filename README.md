d1eb9b401b8e302f976734c14e1040c4e6c99a79
peppeocchi <peppeocchi@gmail.com>
2014-06-08 16:12:40 +0100

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-zoho-books
+Zoho Books API
 ==========
+
+## Zoho Books API requests limit
+
+At the time of writing, the API requests limit is 150 requests per minute per organization
+
+
+## Init
+
+```php
+$zb = new ZohoBooks('your authentication token', 'your organization id');
+```
+
+The above code will set the API start time
+
+==========
+
+## Contacts
+
+### Get all contacts
+
+```php
+$contacts = $zb->allContacts();
+```
+
+### Get contact by ID
+
+```php
+$contact = $zb->getContact(contact_id);
+```
+
+These functions will return a json string if success, false in case of failure
+
+==========
+
+## Invoices
+
+### Get all invoices
+
+```php
+$invoices = $zb->allInvoices();
+```
+
+### Get invoice by ID
+
+```php
+$invoice = $zb->getInvoice(invoice_id);
+```
+
+These functions will return a json string if success, false in case of failure
+
+### Create an invoice
+
+```php
+$zb->postInvoice('invoice_json', true);
+```
+
+This function will return true if success, false in case of failure
+The second parameter is optional, the default value is false, if set to true will send the invoice to the customer
+
+==========
+
+## Credit notes
+
+### Get all credit notes
+
+```php
+$creditNotes = $zb->allCreditNotes();
+```
+
+### Get credit note by ID
+
+```php
+$creditNote = $zb->getCreditNote(creditNote_id);
+```
+
+These functions will return a json string if success, false in case of failure
+
+### Create a credit note
+
+```php
+$zb->postCreditNote('creditNote_json');
+```
+
+This function will return true if success, false in case of failure
+
+==========
+
+## Response HTTP Code
+
+```php
+$httpCode = $zb->getHttpCode();
+```
+
+This function will return the response HTTP code after an API call.
---
+++ ZohoBooks.php
@@ -0,0 +1,296 @@
+<?php
+
+/**
+ *
+ * Zoho Books API
+ * Version: 1.0
+ *
+ * Author: Giuseppe Occhipinti - https://github.com/peppeocchi
+ *
+ */
+
+class ZohoBooks
+{
+	/**
+	 * cUrl timeout
+	 */
+	private $timeout = 30;
+
+	/**
+	 * HTTP code of the cUrl request
+	 */
+	private $httpCode;
+
+	/**
+	 * Zoho Books API authentication
+	 */
+	private $authtoken;
+	private $organizationId;
+
+	/**
+	 * Zoho Books API request limit management
+	 */
+	private $apiRequestsLimit = 150;
+	private $apiRequestsCount;
+	private $apiTimeLimit = 60;
+	private $startTime;
+
+	/**
+	 * Zoho Books API urls request
+	 */
+	private $apiUrl = 'https://books.zoho.com/api/v3/';
+	private $contactsUrl = 'contacts/';
+	private $invoicesUrl = 'invoices/';
+	private $creditnotesUrl = 'creditnotes/';
+
+	
+
+	/**
+	 * Init
+	 *
+	 * @param (string) Zoho Books authentication token
+	 * @param (string) Zoho Books organization id
+	 */
+	public function __construct($authtoken, $organizationId)
+	{
+		$this->authtoken = $authtoken;
+		$this->organizationId = $organizationId;
+		$this->apiRequestsCount = 0;
+		$this->startTime = time();
+	}
+
+
+	/**
+	 * Get all contacts
+	 *
+	 * @return (string) json string || false
+	 */
+	public function allContacts()
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $this->contactsUrl . '?authtoken=' . $this->authtoken . '&organization_id=' . $this->organizationId);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		$contacts = curl_exec($ch);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 200 ? $contacts : false;
+	}
+
+	/**
+	 * Get contact details by ID
+	 *
+	 * @param (int) contact id
+	 *
+	 * @return (string) json string || false
+	 */
+	public function getContact($id)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $this->contactsUrl . $id . '?authtoken=' . $this->authtoken . '&organization_id=' . $this->organizationId);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		$contact = curl_exec($ch);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 200 ? $contact : false;
+	}
+
+
+	/**
+	 * Get all invoices
+	 *
+	 * @return (string) json string || false
+	 */
+	public function allInvoices()
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $this->invoicesUrl . '?authtoken=' . $this->authtoken . '&organization_id=' . $this->organizationId);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		$invoices = curl_exec($ch);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 200 ? $invoices : false;
+	}
+
+
+	/**
+	 * Get invoice
+	 *
+	 * @param (int) invoice id
+	 *
+	 * @return (string) json string || false
+	 */
+	public function getInvoice($id)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $this->invoicesUrl . $id . '?authtoken=' . $this->authtoken . '&organization_id=' . $this->organizationId);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		$invoice = curl_exec($ch);
+		var_dump($invoice);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 200 ? $invoice : false;
+	}
+
+
+	/**
+	 * Create an invoice
+	 *
+	 * @param (string) json encoded
+	 * @param (bool) send the invoice to the contact associated with the invoice
+	 *
+	 * @return (bool)
+	 */
+	public function postInvoice($invoice, $send = false)
+	{
+		$url = $this->apiUrl . $this->invoicesUrl;
+
+		$data = array(
+			'authtoken' 		=> $this->authtoken,
+			'JSONString' 		=> $invoice,
+			"organization_id" 	=> $this->organizationId
+		);
+
+		$ch = curl_init($url);
+
+		curl_setopt_array($ch, array(
+			CURLOPT_POST => 1,
+			CURLOPT_POSTFIELDS => $data,
+			CURLOPT_RETURNTRANSFER => true
+		));
+
+		$invoice = curl_exec($ch);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 201 ? true : false;
+	}
+
+
+	/**
+	 * Get all credit notes
+	 *
+	 * @return (string) json string || false
+	 */
+	public function allCreditNotes()
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $this->creditnotesUrl . '?authtoken=' . $this->authtoken . '&organization_id=' . $this->organizationId);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		$creditnotes = curl_exec($ch);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 200 ? $creditnotes : false;
+	}
+
+
+	/**
+	 * Get credit note
+	 *
+	 * @param (int) credit note id
+	 *
+	 * @return (string) json string || false
+	 */
+	public function getCreditNote($id)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $this->creditnotesUrl . $id . '?authtoken=' . $this->authtoken . '&organization_id=' . $this->organizationId);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		$creditnote = curl_exec($ch);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 200 ? $creditnote : false;
+	}
+
+
+	/**
+	 * Create a credit note
+	 *
+	 * @param (string) json string
+	 *
+	 * @return (bool)
+	 */
+	public function postCreditNote($creditnote)
+	{
+		$url = $this->apiUrl . $this->creditnotesUrl;
+
+		$data = array(
+			'authtoken' 		=> $this->authtoken,
+			'JSONString' 		=> $creditnote,
+			"organization_id" 	=> $this->organizationId
+		);
+
+		$ch = curl_init($url);
+
+		curl_setopt_array($ch, array(
+			CURLOPT_POST => 1,
+			CURLOPT_POSTFIELDS => $data,
+			CURLOPT_RETURNTRANSFER => true
+		));
+
+		$creditnote = curl_exec($ch);
+		$this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		$this->checkApiRequestsLimit();
+
+		return $this->httpCode == 201 ? true : false;
+	}
+
+
+	/**
+	 * Get HTTP code
+	 */
+	public function getHttpCode()
+	{
+		return $this->httpCode ? $this->httpCode : false;
+	}
+
+
+	/**
+	 * Check API requests limit
+	 *
+	 */
+	private function checkApiRequestsLimit()
+	{
+		$tempTime = time() - $this->startTime;
+		if($this->apiRequestsCount >= $this->apiRequestsLimit && $tempTime < $this->apiTimeLimit) {
+			usleep(($this->apiTimeLimit - $tempTime)*1000000);
+			$this->apiRequestsCount = 1;
+			$this->startTime = time();
+		} else {
+			$this->apiRequestsCount++;
+		}
+	}
+}
